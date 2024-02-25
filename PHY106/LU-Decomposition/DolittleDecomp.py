1a7dfb55e453ca3463a2b70b06905110c1ec3332
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-25 15:24:27 +0530

    removed unneccessary prints

--- PHY106/LU-Decomposition/DolittleDecomp.py
+++ PHY106/LU-Decomposition/DolittleDecomp.py
@@ -4,9 +4,7 @@ def DolittleDecomposition(matrix:list, dim:int)->tuple:
     '''returns a tuple containing the decomposed forms of the provided matrix, by Dolittle\'s method'''
     upper = copyMatrix(matrix, dim)
     lower = createTriangularMatrix(dim, 'lower')
-    printMatrix(lower)
-    printMatrix(matrix)
-    printMatrix(upper)
+    
     for i in range(1, dim):
         for j in range(i):
             subFactor = upper[i][j]/upper[j][j]
