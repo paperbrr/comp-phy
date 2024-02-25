23f747ccb0a3552fb4bf00b8fe76c335861855b0
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-25 15:03:26 +0530

    docstrings added

--- PHY106/Misc/heron.py
+++ PHY106/Misc/heron.py
@@ -1,12 +1,11 @@
-def mod(num: int) -> int :
-    return -1*num if num<0 else num
-
-def heronFunc(sqrtOf: int, err: float, maxItCount: int) -> float :
+def HeronRoot(sqrtOf: int, err: float, maxItCount: int) -> float :
+    "finds root of given integer withint maxItCount iterations and/or below given error. 0 return value indicates no root found within iteration count"
     rootNum = sqrtOf/2
     currentItCount = 1
     while currentItCount<maxItCount:
-        if mod(sqrtOf - rootNum**2) <= err:
+        if abs(sqrtOf - rootNum**2) <= err:
             return rootNum
         else: 
             rootNum = 1/2*(rootNum+(sqrtOf/rootNum))
         currentItCount+=1
+    return 0
