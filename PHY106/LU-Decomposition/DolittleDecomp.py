200bb25ab236c969a7e0abe163652b73e9474c5b
PaperBrr <120294723+paperbrr@users.noreply.github.com>
2024-02-25 16:18:55 +0530

    added function to find sols using dolittle

--- PHY106/LU-Decomposition/DolittleDecomp.py
+++ PHY106/LU-Decomposition/DolittleDecomp.py
@@ -4,7 +4,7 @@ def DolittleDecomposition(matrix:list, dim:int)->tuple:
     '''returns a tuple containing the decomposed forms of the provided matrix, by Dolittle\'s method'''
     upper = copyMatrix(matrix, dim)
     lower = createTriangularMatrix(dim, 'lower')
-    
+
     for i in range(1, dim):
         for j in range(i):
             subFactor = upper[i][j]/upper[j][j]
@@ -13,3 +13,13 @@ def DolittleDecomposition(matrix:list, dim:int)->tuple:
                 upper[i][k] -= subFactor*upper[j][k]
 
     return lower, upper
+
+def findSolsDolittle(matrix, ans, dims):
+    '''find the solutions for the given matrix using dolittle\'s decomposition'''
+    lower, upper = DolittleDecomposition(matrix, dims)
+    LY = createAugmentedMatrix(lower, dims, ans)
+    Y = forwardSubstitute(LY, dims)
+    UX = createAugmentedMatrix(upper, dims, Y)
+    X = backSubstitute(UX, dims)
+
+    return X
